87fe498edf462cc692afd8a645d9bacb5d00353c
Math012 <math111.0811@gmail.com>
2025-08-07 16:14:10 -0300

    Adicionando a entidade UserEntity e implementando a interface UserDetails

---
+++ src/main/java/com/math012/authentication_service/infra/model/UserEntity.java
@@ -0,0 +1,37 @@
+package com.math012.authentication_service.infra.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity(name = "user_entity")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "email",unique = true)
+    private String email;
+    @Column(name = "password")
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
