7ace3823c00c0e4b7b3ab362bd6818f03098dcde
Math012 <math111.0811@gmail.com>
2025-08-08 16:53:55 -0300

    Adicionando as dependências do JWT.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,10 @@ dependencies {
 	implementation("org.mapstruct:mapstruct:1.5.3.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
 	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 	//
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
